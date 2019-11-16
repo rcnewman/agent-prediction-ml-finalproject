064db77809a342ee576af50da7b82a4ee0c8b3ba
HamsterHerder <HamsterHerder@github.com>
2019-11-16 16:44:36 -0600

    model_001 mostly working

---
+++ Model_001/class_model.py
@@ -0,0 +1,47 @@
+# Imports ---------------------------------------------------------------------
+from sklearn.svm import SVC
+
+# Model Class -----------------------------------------------------------------
+class Model():
+    # Class variables.
+    training_data = None
+    training_labels = None
+    model_name = None
+    model = None
+    
+    def __init__(self, data, model_name):
+        # Initilzie class variables.
+        self.training_data = []
+        self.training_labels = []
+        label_idx = 0
+        if model_name == "p":
+            label_idx = 0
+        if model_name == "a":
+            label_idx = 1
+        for doc in data:
+            for row in doc.get_features():
+                self.training_data.append(row[1:3])
+            self.training_labels.extend(doc.get_gold_labels())
+        print(self.training_data)
+    
+    def train_model(self, parameters):
+        clf = SVC(**parameters)
+        clf.fit(self.training_data, self.training_labels)
+        self.model = clf
+    
+    def predict(self, doc):
+        return self.model.predict(doc)
+    
+    def cross_validate(self):
+        pass
+    
+    def test(self, doc, label):
+        p = self.model.predict(doc)
+        if p in label or label in p:
+            return True
+        return False
+    
+    
+    
+# probability = True,
+#                  kernel = "linear"
--- Model_001/main.py
+++ Model_001/main.py
@@ -1,36 +1,38 @@
 # Installs and Downloads ------------------------------------------------------
 import pandas as pd
 from class_document import Document
+from class_model import Model
+import pickle
 
+# Get Data --------------------------------------------------------------------
 all_docs = []
-accs = []
 with open("../data_labels.csv", "rb") as file:
     labels = pd.read_csv(file)
     for i, row in labels.iterrows():
         try:
             all_docs.append(Document(row["Book"], "../Files/"+row["file"], row["Protagonist"], row["Antagonist"]))
-            x = [all_docs[-1].title]
-            x.extend(all_docs[-1].preds())
-            accs.append(x)
+#            with open("../Dataset/%s.pickle"%(row["Book"]), "wb") as file:
+#                pickle.dump(all_docs[-1], file)
         except Exception as e:
             print("Error: No file found "+row["file"]+".")
             print(e)
             print()
+        if len(all_docs) > 1:
+            break
 
-pa = 0
-aa = 0
-for row in accs:
-    if row[1]:
-        pa += 1
-    if row[2]:
-        aa += 1
-pa = pa/len(accs)
-aa = aa/len(accs)
-print("protagonist accuracy: "+str(pa))
-print("antagonist accuracy: "+str(aa))
-with open("../Results/baseline.txt", "w") as file:
-#    file.writelines(accs)
-    for row in accs:
-        file.write("%s, %s, %s\n"%(row[0], row[1], row[2]))
-    file.write("protagonist accuracy: %s\n"%(str(pa)))
-    file.write("antagonist accuracy: %s\n"%(str(aa)))
+# Train Models ----------------------------------------------------------------
+params = {
+            "probability": True,
+            "kernel": "linear"
+            }
+print("models")
+p_model = Model(all_docs, "p")
+print("built model")
+p_model.train_model(params)
+print("Trained p model")
+a_model = Model(all_docs, "a")
+a_model.train_model(params)
+print("Trained pa model")
+
+# Get Test Accuracy -----------------------------------------------------------
+            
