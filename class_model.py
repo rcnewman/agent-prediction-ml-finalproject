fd35e17fc3e5da36661ac5ec00d4eab16d5ad21e
Ross Newman <rosscnewman@gmail.com>
2019-11-20 23:13:19 -0600

    Grid Search MLP results

--- class_model.py
+++ class_model.py
@@ -4,6 +4,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn import tree, metrics
 from sklearn.model_selection import GridSearchCV
+import numpy as np
+from sklearn.ensemble import AdaBoostClassifier
 
 # Model Class -----------------------------------------------------------------
 class Model_SVM():
@@ -55,13 +57,13 @@ class Model_SVM():
                 test_data.append(b[1:])
             test_labels.extend(row.get_gold_labels())
             
-#            p = self.model.predict_proba([row[1:]])[0]
-#            if best_p[1] < p[2]:
-#                best_p = [row[0], p[2]]
-#            if best_a[1] < p[0]:
-#                best_a = [row[0], p[0]]
-#                
-#            a.append([p,row[0]])
+            p = self.model.predict_proba([row[1:]])[0]
+            if best_p[1] < p[2]:
+                best_p = [row[0], p[2]]
+            if best_a[1] < p[0]:
+                best_a = [row[0], p[0]]
+                
+            a.append([p,row[0]])
         y_pred = self.model.predict(test_data)
         print("SVM Confusion Matrix")
         print(metrics.confusion_matrix(y_true=test_labels, y_pred=y_pred))
@@ -112,7 +114,13 @@ class Model_NB():
         best_p = [None, 0]
         best_a = [None, 0]
         a = []
+        test_data = []
+        test_labels = []
         for row in doc:
+            for b in row.get_features():
+                test_data.append(b[1:])
+            test_labels.extend(row.get_gold_labels())
+            
             p = self.model.predict_proba([row[1:]])[0]
             if best_p[1] < p[2]:
                 best_p = [row[0], p[2]]
@@ -120,8 +128,14 @@ class Model_NB():
                 best_a = [row[0], p[0]]
                 
             a.append([p,row[0]])
-        sorted_list_a = sorted(a,key= lambda x: x[0][0])
-        sorted_list_p = sorted(a,key=lambda x: x[0][2])
+        y_pred = self.model.predict(test_data)
+        print("NB Confusion Matrix")
+        print(metrics.confusion_matrix(y_true=test_labels, y_pred=y_pred))
+        print()
+        print("NB Classification Report")
+        print(metrics.classification_report(test_labels, y_pred))
+#        sorted_list_a = sorted(a,key= lambda x: x[0][0])
+#        sorted_list_p = sorted(a,key=lambda x: x[0][2])
         return [best_p, best_a]
     
     def test(self, test_data):
@@ -159,7 +173,7 @@ class Model_MLP():
     def train_model(self, parameters):
         clf = MLPClassifier(**parameters)
         grid_params = {'alpha': 10.0 ** -np.arange(1, 10), 'hidden_layer_sizes':np.arange(10, 15)}
-        mlp_grid = GridSearchCV(clf, grid_params,cv=3)
+        mlp_grid = GridSearchCV(clf, grid_params,cv=3,verbose=3, n_jobs=4)
         print("Training MLP...")
         mlp_grid.fit(self.training_data, self.training_labels)
 #        clf.fit(self.training_data, self.training_labels)
@@ -174,7 +188,13 @@ class Model_MLP():
         best_p = [None, 0]
         best_a = [None, 0]
         a = []
+        test_data = []
+        test_labels = []
         for row in doc:
+            for b in row.get_features():
+                test_data.append(b[1:])
+            test_labels.extend(row.get_gold_labels())
+            
             p = self.model.predict_proba([row[1:]])[0]
             if best_p[1] < p[2]:
                 best_p = [row[0], p[2]]
@@ -182,9 +202,12 @@ class Model_MLP():
                 best_a = [row[0], p[0]]
                 
             a.append([p,row[0]])
-        sorted_list_a = sorted(a,key= lambda x: x[0][0])
-        sorted_list_p = sorted(a,key=lambda x: x[0][2])
-        return [best_p, best_a]
+        y_pred = self.model.predict(test_data)
+        print("MLP Confusion Matrix")
+        print(metrics.confusion_matrix(y_true=test_labels, y_pred=y_pred))
+        print()
+        print("MLP Classification Report")
+        print(metrics.classification_report(test_labels, y_pred))
     
     def test(self, test_data):
         acc = 0.0
@@ -218,7 +241,7 @@ class Model_Tree():
             self.training_labels.extend(doc.get_gold_labels())
     
     def train_model(self, parameters):
-        clf = tree.DecisionTreeClassifier(**parameters)
+        clf = tree.AdaBoostClassifier(**parameters)
         clf.fit(self.training_data, self.training_labels)
         self.model = clf
     
@@ -247,3 +270,70 @@ class Model_Tree():
                 data.append(row[1:])
             labels.extend(doc.get_gold_labels())
         return self.model.score(data, labels)
+    
+    
+class Model_AdaBoost():
+    # Class variables.
+    training_data = None
+    training_labels = None
+    model_name = None
+    model = None
+    for_csv = []
+    
+    def __init__(self, data):
+        # Initilzie class variables.
+        self.training_data = []
+        self.training_labels = []
+        self.grid_results = []
+        x = []
+        y = []
+        for doc in data:
+            for row in doc.get_features():
+                self.training_data.append(row[1:])
+                x.append(row[1])
+                y.append(row[2])
+            self.training_labels.extend(doc.get_gold_labels())
+    
+    def train_model(self, parameters):
+        clf = AdaBoostClassifier(**parameters)
+        print("Training Adaboost...")
+        clf.fit(self.training_data, self.training_labels)
+        self.model = clf
+    
+    def predict(self, doc):
+        best_p = [None, 0]
+        best_a = [None, 0]
+        a = []
+        test_data = []
+        test_labels = []
+        for row in doc:
+            for b in row.get_features():
+                test_data.append(b[1:])
+            test_labels.extend(row.get_gold_labels())
+            
+            p = self.model.predict_proba([row[1:]])[0]
+            if best_p[1] < p[2]:
+                best_p = [row[0], p[2]]
+            if best_a[1] < p[0]:
+                best_a = [row[0], p[0]]
+                
+            a.append([p,row[0]])
+        y_pred = self.model.predict(test_data)
+        print("AdaBoost Confusion Matrix")
+        print(metrics.confusion_matrix(y_true=test_labels, y_pred=y_pred))
+        print()
+        print("AdaBoost Classification Report")
+        print(metrics.classification_report(test_labels, y_pred))
+    
+    def test(self, test_data):
+        acc = 0.0
+        data = []
+        labels = []
+        for doc in test_data:
+            for row in doc.get_features():
+                data.append(row[1:])
+            labels.extend(doc.get_gold_labels())
+        return self.model.score(data, labels)
+
+    
+    
--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 # Installs and Downloads ------------------------------------------------------
 import pandas as pd
 from class_document import Document
-from class_model import Model_SVM, Model_NB, Model_MLP, Model_Tree
+from class_model import Model_SVM, Model_NB, Model_MLP, Model_Tree, Model_AdaBoost
 import pickle
 import os
 from sklearn.model_selection import train_test_split
 
 # Get Data --------------------------------------------------------------------
-#all_docs = []
+all_docs = []
 
+with open("data_labels_2.csv", "rb") as file:
 #with open("data_labels_2.csv", "rb") as file:
-##with open("data_labels_2.csv", "rb") as file:
-#    labels = pd.read_csv(file)
-#    for i, row in labels.iterrows():
-#        print(i)
-#        try:
-#            all_docs.append(Document(row["Book"], "Files/"+row["file"], row["Protagonist"], row["Antagonist"]))
-##            with open("Dataset/%s.pickle"%(row["Book"]), "wb") as file:
+    labels = pd.read_csv(file)
+    for i, row in labels.iterrows():
+        print(i)
+        try:
+            all_docs.append(Document(row["Book"], "Files/"+row["file"], row["Protagonist"], row["Antagonist"]))
 #            with open("Dataset/%s.pickle"%(row["Book"]), "wb") as file:
-#                pickle.dump(all_docs[-1], file)
-#        except Exception as e:
-#            print("Error: No file found "+row["file"]+".")
-#            print(e)
-#            print()
-#        print("File %d Complete"%(i))
+            with open("Dataset/%s.pickle"%(row["Book"]), "wb") as file:
+                pickle.dump(all_docs[-1], file)
+        except Exception as e:
+            print("Error: No file found "+row["file"]+".")
+            print(e)
+            print()
+        print("File %d Complete"%(i))
 
-#for file in os.listdir("Dataset"):
-#    filename = os.fsdecode(file)
-#    if filename.endswith(".pickle"):
-#        print(filename)
-##        print(str(os.path.getsize("Dataset/" + filename)))
-#        try:
-#            with open("Dataset/" + filename,"rb") as picklefile:
-#                doc = pickle.load(picklefile)
-#                all_docs.append(doc)
-#        except Exception as e:
-#            print(e)
+for file in os.listdir("Dataset"):
+    filename = os.fsdecode(file)
+    if filename.endswith(".pickle"):
+        print(filename)
+#        print(str(os.path.getsize("Dataset/" + filename)))
+        try:
+            with open("Dataset/" + filename,"rb") as picklefile:
+                doc = pickle.load(picklefile)
+                all_docs.append(doc)
+        except Exception as e:
+            print(e)
 
 train_data = []
 test_data = []
@@ -57,39 +57,53 @@ model_svm = Model_SVM(train_data)
 model_svm.train_model(params_svm)
 with open("Models/svm.pickle", "wb") as file:
     pickle.dump(model_svm, file)
-#    
-#params_nb = {
-#            }
-#model_nb = Model_NB(train_data)
-#model_nb.train_model(params_nb)
-#with open("Models/nb.pickle", "wb") as file:
-#    pickle.dump(model_nb, file)
-#    
-#params_mlp = {
-#            }
-#model_mlp = Model_MLP(train_data)
-#model_mlp.train_model(params_mlp)
-#with open("Models/mlp.pickle", "wb") as file:
-#    pickle.dump(model_mlp, file)
-#    
-#params_tree = {
-#            }
-#model_tree = Model_Tree(train_data)
-#model_tree.train_model(params_tree)
-#with open("Models/tree.pickle", "wb") as file:
-#    pickle.dump(model_tree, file)
+    
+params_nb = {
+            }
+model_nb = Model_NB(train_data)
+model_nb.train_model(params_nb)
+with open("Models/nb.pickle", "wb") as file:
+    pickle.dump(model_nb, file)
+    
+params_mlp = {
+        "verbose": True
+            }
+model_mlp = Model_MLP(train_data)
+model_mlp.train_model(params_mlp)
+with open("Models/mlp.pickle", "wb") as file:
+    pickle.dump(model_mlp, file)
+    
+params_tree = {
+            }
+model_tree = Model_Tree(train_data)
+model_tree.train_model(params_tree)
+with open("Models/tree.pickle", "wb") as file:
+    pickle.dump(model_tree, file)
     
 print("SVM:")
 results_svm = model_svm.test(test_data)
 
 predict_svm = model_svm.predict(test_data)
-#print()
-#print("NB:")
-#print(model_nb.test(test_data))
-#print()
-#print("MLP:")
-#print(model_mlp.test(test_data))
-#print()
-#print("Tree:")
-#print(model_tree.test(test_data))
+print()
+print("NB:")
+results_nb = model_nb.test(test_data)
+predict_nb = model_nb.predict(test_data)
+print()
+print("MLP:")
+results_mlp = model_mlp.test(test_data)
+predict_mlp = model_mlp.predict(test_data)
+
+print("Tree:")
+print(model_tree.test(test_data))
+
+params_ab = { "n_estimators":10
+            }
+model_ab = Model_AdaBoost(train_data)
+model_ab.train_model(params_ab)
+with open("Models/ab.pickle", "wb") as file:
+    pickle.dump(model_ab, file)
+    
+print("Adaboost:")
+results_ab = model_ab.test(test_data)
+predict_ab = model_ab.predict(test_data)
 print()
