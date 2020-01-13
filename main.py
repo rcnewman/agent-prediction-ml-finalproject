90c2cc376055cc38f10f40eb0d516733d810bbbe
HamsterHerder <HamsterHerder@github.com>
2020-01-13 11:38:55 -0600

    Update

--- main.py
+++ main.py
@@ -7,103 +7,111 @@ import os
 from sklearn.model_selection import train_test_split
 
 # Get Data --------------------------------------------------------------------
-all_docs = []
-
-with open("data_labels_2.csv", "rb") as file:
+#all_docs = []
+#
 #with open("data_labels_2.csv", "rb") as file:
-    labels = pd.read_csv(file)
-    for i, row in labels.iterrows():
-        print(i)
-        try:
-            all_docs.append(Document(row["Book"], "Files/"+row["file"], row["Protagonist"], row["Antagonist"]))
+##with open("data_labels_2.csv", "rb") as file:
+#    labels = pd.read_csv(file)
+#    for i, row in labels.iterrows():
+#        print(i)
+#        try:
+#            all_docs.append(Document(row["Book"], "Files/"+row["file"], row["Protagonist"], row["Antagonist"]))
+##            with open("Dataset/%s.pickle"%(row["Book"]), "wb") as file:
 #            with open("Dataset/%s.pickle"%(row["Book"]), "wb") as file:
-            with open("Dataset/%s.pickle"%(row["Book"]), "wb") as file:
-                pickle.dump(all_docs[-1], file)
-        except Exception as e:
-            print("Error: No file found "+row["file"]+".")
-            print(e)
-            print()
-        print("File %d Complete"%(i))
+#                pickle.dump(all_docs[-1], file)
+#        except Exception as e:
+#            print("Error: No file found "+row["file"]+".")
+#            print(e)
+#            print()
+#        print("File %d Complete"%(i))
+#
+#for file in os.listdir("Dataset"):
+#    filename = os.fsdecode(file)
+#    if filename.endswith(".pickle"):
+#        print(filename)
+##        print(str(os.path.getsize("Dataset/" + filename)))
+#        try:
+#            with open("Dataset/" + filename,"rb") as picklefile:
+#                doc = pickle.load(picklefile)
+#                all_docs.append(doc)
+#        except Exception as e:
+#            print(e)
+#
+#train_data = []
+#test_data = []
+#for i, doc in enumerate(all_docs):
+#    if i < int(len(all_docs)*0.8)+1:
+#        train_data.append(doc)
+#    else:
+#        test_data.append(doc)
+#
+## Train Models ---------------------------------------------------------------
+#params_svm = {
+#            "probability": True,
+#            "kernel": "rbf",
+#            "class_weight": "balanced",
+#            "gamma": "auto",
+#            "verbose": True
+#            }
+#model_svm = Model_SVM(train_data)
+#model_svm.train_model(params_svm)
+#with open("Models/svm.pickle", "wb") as file:
+#    pickle.dump(model_svm, file)
+#    
+#params_nb = {
+#            }
+#model_nb = Model_NB(train_data)
+#model_nb.train_model(params_nb)
+#with open("Models/nb.pickle", "wb") as file:
+#    pickle.dump(model_nb, file)
+#    
+#params_mlp = {
+#        "verbose": True
+#            }
+#model_mlp = Model_MLP(train_data)
+#model_mlp.train_model(params_mlp)
+#with open("Models/mlp.pickle", "wb") as file:
+#    pickle.dump(model_mlp, file)
+#    
+#params_tree = {
+#            }
+#model_tree = Model_Tree(train_data)
+#model_tree.train_model(params_tree)
+#with open("Models/tree.pickle", "wb") as file:
+#    pickle.dump(model_tree, file)
+#    
+#print("SVM:")
+#results_svm = model_svm.test(test_data)
+#
+#predict_svm = model_svm.predict(test_data)
+#print()
+#print("NB:")
+#results_nb = model_nb.test(test_data)
+#predict_nb = model_nb.predict(test_data)
+#print()
+#print("MLP:")
+#results_mlp = model_mlp.test(test_data)
+#predict_mlp = model_mlp.predict(test_data)
+#
+#print("Tree:")
+#print(model_tree.test(test_data))
+#
+#params_ab = { "n_estimators":10
+#            }
+#model_ab = Model_AdaBoost(train_data)
+#model_ab.train_model(params_ab)
+#with open("Models/ab.pickle", "wb") as file:
+#    pickle.dump(model_ab, file)
+#    
+#print("Adaboost:")
+#results_ab = model_ab.test(test_data)
+#predict_ab = model_ab.predict(test_data)
+#print()
 
-for file in os.listdir("Dataset"):
-    filename = os.fsdecode(file)
-    if filename.endswith(".pickle"):
-        print(filename)
-#        print(str(os.path.getsize("Dataset/" + filename)))
-        try:
-            with open("Dataset/" + filename,"rb") as picklefile:
-                doc = pickle.load(picklefile)
-                all_docs.append(doc)
-        except Exception as e:
-            print(e)
-
-train_data = []
-test_data = []
-for i, doc in enumerate(all_docs):
-    if i < int(len(all_docs)*0.8)+1:
-        train_data.append(doc)
-    else:
-        test_data.append(doc)
-
-# Train Models ---------------------------------------------------------------
 params_svm = {
             "probability": True,
-            "kernel": "rbf",
-            "class_weight": "balanced",
-            "gamma": "auto",
-            "verbose": True
             }
 model_svm = Model_SVM(train_data)
 model_svm.train_model(params_svm)
-with open("Models/svm.pickle", "wb") as file:
-    pickle.dump(model_svm, file)
-    
-params_nb = {
-            }
-model_nb = Model_NB(train_data)
-model_nb.train_model(params_nb)
-with open("Models/nb.pickle", "wb") as file:
-    pickle.dump(model_nb, file)
-    
-params_mlp = {
-        "verbose": True
-            }
-model_mlp = Model_MLP(train_data)
-model_mlp.train_model(params_mlp)
-with open("Models/mlp.pickle", "wb") as file:
-    pickle.dump(model_mlp, file)
-    
-params_tree = {
-            }
-model_tree = Model_Tree(train_data)
-model_tree.train_model(params_tree)
-with open("Models/tree.pickle", "wb") as file:
-    pickle.dump(model_tree, file)
-    
 print("SVM:")
 results_svm = model_svm.test(test_data)
-
-predict_svm = model_svm.predict(test_data)
-print()
-print("NB:")
-results_nb = model_nb.test(test_data)
-predict_nb = model_nb.predict(test_data)
-print()
-print("MLP:")
-results_mlp = model_mlp.test(test_data)
-predict_mlp = model_mlp.predict(test_data)
-
-print("Tree:")
-print(model_tree.test(test_data))
-
-params_ab = { "n_estimators":10
-            }
-model_ab = Model_AdaBoost(train_data)
-model_ab.train_model(params_ab)
-with open("Models/ab.pickle", "wb") as file:
-    pickle.dump(model_ab, file)
-    
-print("Adaboost:")
-results_ab = model_ab.test(test_data)
-predict_ab = model_ab.predict(test_data)
-print()
